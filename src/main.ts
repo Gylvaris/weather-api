6b7f64898831d286d9ccaf3f83ab978c4e736adf
Gylvaris <111581734+Gylvaris@users.noreply.github.com>
2024-10-18 19:29:34 +0200

    add rate limiter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { rateLimiter } from 'hono-rate-limiter';
 
 import { getWeatherData } from './getWeather';
+import { getConnInfo } from '@hono/node-server/conninfo';
 
 const app = new Hono();
+
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-6', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: c => c.req.header('x-forwarded-for') ?? getConnInfo(c).remote.address ?? 'unknown', // Method to generate custom identifiers for clients.
+  //store: memoryStore(), // Redis, MemoryStore, etc. See below.
+});
+
+app.use('/weather/*', limiter);
 
 app.get('/', r => r.json({ message: 'Here u can check your weather!' }));
 app.get('/weather/:city', r =>
